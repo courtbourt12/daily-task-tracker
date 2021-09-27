52f0574019d7e9eaece615e965c64047300e8967
courtney long <courtneylong877@gmail.com>
2021-09-27 14:56:20 -0400

    The program works properly

--- structure.js
+++ structure.js
@@ -5,7 +5,15 @@ $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 // Setting variables for the textboxes and buttons.
 
-var allButtons = document.querySelectorAll("#push");
+var allButtons9 = document.querySelector("#push9");
+var allButtons10 = document.querySelector("#push10");
+var allButtons11 = document.querySelector("#push11");
+var allButtons12 = document.querySelector("#push12");
+var allButtons1 = document.querySelector("#push1");
+var allButtons2 = document.querySelector("#push2");
+var allButtons3 = document.querySelector("#push3");
+var allButtons4 = document.querySelector("#push4");
+var allButtons5 = document.querySelector("#push5");
 
 var textbox9 = document.getElementById("text9");
 var textbox10 = document.getElementById("text10");
@@ -91,5 +99,101 @@ if (currentTime==scheduleHours9) {
     textbox4.style.backgroundColor = "gray";
     textbox5.style.backgroundColor = "red";
 }
+
+// Functions for retreiving and storing the events from local storage.
+
+var retreive9 = localStorage.getItem("Event of the Day 9");
+console.log(retreive9);
+textbox9.value = retreive9;
+
+var retreive10 = localStorage.getItem("Event of the Day 10");
+console.log(retreive10);
+textbox10.value = retreive10;
+
+var retreive11 = localStorage.getItem("Event of the Day 11");
+console.log(retreive11);
+textbox11.value = retreive11;
+
+var retreive12 = localStorage.getItem("Event of the Day 12");
+console.log(retreive12);
+textbox12.value = retreive12;
+
+var retreive1 = localStorage.getItem("Event of the Day 1");
+console.log(retreive1);
+textbox1.value = retreive1;
+
+var retreive2 = localStorage.getItem("Event of the Day 2");
+console.log(retreive2);
+textbox2.value = retreive2;
+
+var retreive3 = localStorage.getItem("Event of the Day 3");
+console.log(retreive3);
+textbox3.value = retreive3;
+
+var retreive4 = localStorage.getItem("Event of the Day 4");
+console.log(retreive4);
+textbox4.value = retreive4;
+
+var retreive5 = localStorage.getItem("Event of the Day 5");
+console.log(retreive5);
+textbox5.value = retreive5;
+
+// Functions for logging the events into local storage.
+
+var storage9 = function () {
+    var value9 = localStorage.setItem("Event of the Day 9", textbox9.value);
+    console.log(value9);
+}
+
+var storage10 = function () {
+    var value10 = localStorage.setItem("Event of the Day 10", textbox10.value);
+    console.log(value10);
+}
+
+var storage11 = function () {
+    var value11 = localStorage.setItem("Event of the Day 11", textbox11.value);
+    console.log(value11);
+}
+
+var storage12 = function () {
+    var value12 = localStorage.setItem("Event of the Day 12", textbox12.value);
+    console.log(value12);
+}
+
+var storage1 = function () {
+    var value1 = localStorage.setItem("Event of the Day 1", textbox1.value);
+    console.log(value1);
+}
+var storage2 = function () {
+    var value2 = localStorage.setItem("Event of the Day 2", textbox2.value);
+    console.log(value2);
+}
+
+var storage3 = function () {
+    var value3 = localStorage.setItem("Event of the Day 3", textbox3.value);
+    console.log(value3);
+}
+
+var storage4 = function () {
+    var value4 = localStorage.setItem("Event of the Day 4", textbox4.value);
+    console.log(value4);
+}
+
+var storage5 = function () {
+    var value5 = localStorage.setItem("Event of the Day 5", textbox5.value);
+    console.log(value5);
+}
+
+// Adding click events to save log the internal text to local storage.
+
+allButtons9.addEventListener("click",storage9);
+allButtons10.addEventListener("click",storage10);
+allButtons11.addEventListener("click",storage11);
+allButtons12.addEventListener("click",storage12);
+allButtons1.addEventListener("click",storage1);
+allButtons2.addEventListener("click",storage2);
+allButtons3.addEventListener("click",storage3);
+allButtons4.addEventListener("click",storage4);
+allButtons5.addEventListener("click",storage5);
 
 
